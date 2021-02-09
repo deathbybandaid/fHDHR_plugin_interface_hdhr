f82cdd51e26e53e26c9a2df1ab123bda202a1098
deathbybandaid <github@deathbybandaid.net>
2021-02-09 09:59:07 -0500

    test

--- web/discover_json.py
+++ web/discover_json.py
@@ -12,28 +12,37 @@ class Discover_JSON():
     def __call__(self, *args):
         return self.get(*args)
 
+    @property
+    def source(self):
+        if self.fhdhr.config.dict["hdhr"]["source"]:
+            return self.fhdhr.config.dict["hdhr"]["source"]
+        elif len(self.fhdhr.origins.valid_origins):
+            return self.fhdhr.origins.valid_origins[0]
+        else:
+            return None
+
     def get(self, *args):
 
         base_url = request.url_root[:-1]
 
-        jsondiscover = []
-        for origin in self.fhdhr.origins.valid_origins:
+        if self.source in self.fhdhr.origins.valid_origins:
 
             origindiscover = {
-                                "FriendlyName": "%s %s" % (self.fhdhr.config.dict["fhdhr"]["friendlyname"], origin),
+                                "FriendlyName": "%s %s" % (self.fhdhr.config.dict["fhdhr"]["friendlyname"], self.source),
                                 "Manufacturer": self.fhdhr.config.dict["hdhr"]["reporting_manufacturer"],
                                 "ModelNumber": self.fhdhr.config.dict["hdhr"]["reporting_model"],
                                 "FirmwareName": self.fhdhr.config.dict["hdhr"]["reporting_firmware_name"],
-                                "TunerCount": self.fhdhr.origins.origins_dict[origin].tuners,
+                                "TunerCount": self.fhdhr.origins.origins_dict[self.source].tuners,
                                 "FirmwareVersion": self.fhdhr.config.dict["hdhr"]["reporting_firmware_ver"],
-                                "DeviceID": "%s%s" % (self.fhdhr.config.dict["main"]["uuid"], origin),
+                                "DeviceID": "%s%s" % (self.fhdhr.config.dict["main"]["uuid"], self.source),
                                 "DeviceAuth": self.fhdhr.config.dict["fhdhr"]["device_auth"],
-                                "BaseURL": "%s/hdhr/%s" % (base_url, origin),
-                                "LineupURL": "%s/hdhr/%s/lineup.json" % (base_url, origin)
+                                "BaseURL": "%s/hdhr/%s" % (base_url, self.source),
+                                "LineupURL": "%s/hdhr/%s/lineup.json" % (base_url, self.source)
                             }
-            jsondiscover.append(origindiscover)
+        else:
+            origindiscover = {}
 
-        discover_json = json.dumps(jsondiscover, indent=4)
+        discover_json = json.dumps(origindiscover, indent=4)
 
         return Response(status=200,
                         response=discover_json,
