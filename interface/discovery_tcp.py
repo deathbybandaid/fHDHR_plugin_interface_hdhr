64f11a85295dd3c02debfc1a665408349c3e2b63
deathbybandaid <github@deathbybandaid.net>
2021-02-16 13:52:16 -0500

    test

--- interface/discovery_tcp.py
+++ interface/discovery_tcp.py
@@ -3,10 +3,10 @@ import socket
 
 from .discovery_shared import (HDHR_Discovery_Service_Shared,
                                HDHOMERUN_MAX_PACKET_SIZE, HDHOMERUN_TYPE_DISCOVER_REQ, HDHOMERUN_TYPE_GETSET_REQ,
-                               HDHOMERUN_TYPE_DISCOVER_RPY, HDHOMERUN_DISCOVER_UDP_PORT)
+                               HDHOMERUN_TYPE_DISCOVER_RPY, HDHOMERUN_CONTROL_TCP_PORT)
 
 
-class HDHR_Discovery_Service_UDP():
+class HDHR_Discovery_Service_TCP():
 
     def __init__(self, fhdhr, plugin_utils, interface):
         self.fhdhr = fhdhr
@@ -16,18 +16,19 @@ class HDHR_Discovery_Service_UDP():
         self.discovery_shared = HDHR_Discovery_Service_Shared(fhdhr, plugin_utils, interface)
 
         if self.discovery_shared.hdhr_discovery_address and self.fhdhr.config.dict["hdhr"]["discovery"]:
-            self.fhdhr.logger.info("Initializing HDHR UDP Discovery system")
-            self.fhdhr.threads["hdhr_udp_discovery"] = threading.Thread(target=self.discovery_service_listen)
+            self.fhdhr.logger.info("Initializing HDHR TCP Discovery system")
+            self.fhdhr.threads["hdhr_tcp_discovery"] = threading.Thread(target=self.discovery_service_listen)
             self.setup_discovery()
 
         elif not self.fhdhr.config.dict["hdhr"]["discovery"]:
-            self.fhdhr.logger.info("HDHR UDP Discovery system will not be Initialized: Not Enabled")
+            self.fhdhr.logger.info("HDHR TCP Discovery system will not be Initialized: Not Enabled")
         elif not self.discovery_shared.hdhr_discovery_address:
-            self.fhdhr.logger.info("HDHR UDP Discovery system will not be Initialized: Address not set in [hdhr]discovery_address [ssdp]multicast_address or [fhdhr]discovery_address")
+            self.fhdhr.logger.info("HDHR TCP Discovery system will not be Initialized: Address not set in [hdhr]discovery_address [ssdp]multicast_address or [fhdhr]discovery_address")
         else:
-            self.fhdhr.logger.info("HDHR UDP Discovery system will not be Initialized")
+            self.fhdhr.logger.info("HDHR TCP Discovery system will not be Initialized")
 
     def discovery_service_listen(self):
+        self.sock.listen(1)
         while True:
 
             try:
@@ -65,9 +66,5 @@ class HDHR_Discovery_Service_UDP():
         self.sock.close()
 
     def setup_discovery(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-
-        # Enable broadcasting mode
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.sock.bind(('0.0.0.0', HDHOMERUN_DISCOVER_UDP_PORT))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(("0.0.0.0", HDHOMERUN_CONTROL_TCP_PORT))
