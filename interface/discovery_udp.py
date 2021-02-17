88cef675cc3869e2d9964ff766950fbd4e8195fd
deathbybandaid <github@deathbybandaid.net>
2021-02-17 14:23:10 -0500

    test

--- interface/discovery_udp.py
+++ interface/discovery_udp.py
@@ -32,10 +32,10 @@ class HDHR_Discovery_Service_UDP():
                 self.fhdhr.logger.ssdp('No packet received')
                 break
 
-            print(self.sock.gethostbyaddr())
-
             self.fhdhr.logger.ssdp("Request: %s" % self.discovery_shared.format_packet(packet))
             (packetType, requestPayload) = self.discovery_shared.retrieveTypeAndPayload(packet)
+            print(requestPayload)
+            print(packetType)
 
             if packetType == HDHOMERUN_TYPE_DISCOVER_REQ:
                 self.fhdhr.logger.ssdp("Discovery request received from %s" % str(client))
